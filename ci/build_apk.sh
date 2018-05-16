252cf4bf718d87bd9b10175801bea54b3900ac73
Stephen Cirner <steve@blackfynn.com>
2018-05-16 10:13:50 -0400

    Try full build.

--- ci/build_apk.sh
+++ ci/build_apk.sh
@@ -2,5 +2,5 @@
 
 VERSION=$(./version.sh)
 
-fpm -s dir -t apk -n cloudwrap -v ${VERSION} -a x86_64 target/debug/cloudwrap=/usr/bin
-aws s3 cp target/debug/cloudwrap_${VERSION}_x86_64.apk s3://data.blackfynn.io/public-downloads/cloudwrap/
+fpm -s dir -t apk -n cloudwrap -v ${VERSION} -a x86_64 target/x86_64-unknown-linux-musl/release/cloudwrap=/usr/bin
+aws s3 cp cloudwrap_${VERSION}_x86_64.apk s3://data.blackfynn.io/public-downloads/cloudwrap/
